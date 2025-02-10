82948756e2692a320acfbd3183f70e3886aeca86
robmalonzo <142964106+robmalonzo@users.noreply.github.com>
2025-02-10 19:24:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -47,9 +47,9 @@ function displayCat() {
     // Create a new Image element for the cat
     var catImage = new Image();
     // Set the source (file path) for the cat image
-    catImage.src = 'cat.gif'; // Assuming the cat image is named "cat.gif"
+    catImage.src = 'oia-uia.gif'; // Assuming the cat image is named "cat.gif"
     // Set alternative text for the image (for accessibility)
-    catImage.alt = 'Cat';
+    catImage.alt = 'oia-uia';
     // When the cat image is fully loaded, add it to the image container
     catImage.onload = function() {
         imageContainer.appendChild(catImage);
